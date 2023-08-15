d8fe2a31596d81a5ed5f2ef63272f2b18f9458b3
uccergel <ugurcemcergel@gmail.com>
2023-08-15 22:55:05 +0300

    son düzenlemeler

--- src/pages/personal-info/PersonalList.jsx
+++ src/pages/personal-info/PersonalList.jsx
@@ -1,12 +1,35 @@
 import { useEffect } from 'react'
 import useEmployeesStore from '../../utils/store/EmployeesStore'
 import { Table } from '@mantine/core'
+import { shallow } from 'zustand/shallow'
 
 export default function PersonalListPage() {
-  const { employeesData, getEmployeesData } = useEmployeesStore((state) => ({
-    employeesData: state.employeesData,
-    getEmployeesData: state.getEmployeesData
-  }))
+  const {
+    employeesData,
+    getEmployeesData,
+    educationData,
+    getEducationData,
+    martialStatusData,
+    getMartialStatusData,
+    jobStationData,
+    getJobStationData,
+    bloodGroupData,
+    getBloodGroupData
+  } = useEmployeesStore(
+    (state) => ({
+      employeesData: state.employeesData,
+      getEmployeesData: state.getEmployeesData,
+      educationData: state.educationData,
+      getEducationData: state.getEducationData,
+      martialStatusData: state.martialStatusData,
+      getMartialStatusData: state.getMartialStatusData,
+      jobStationData: state.jobStationData,
+      getJobStationData: state.getJobStationData,
+      bloodGroupData: state.bloodGroupData,
+      getBloodGroupData: state.getBloodGroupData
+    }),
+    shallow
+  )
   const theadItems = (
     <tr>
       <th>Adı Soyadı</th>
@@ -28,15 +51,53 @@ export default function PersonalListPage() {
       <td>{employeeData.eMail}</td>
       <td>{employeeData.phoneNumber}</td>
       <td>{employeeData.birthDate}</td>
+      <td>
+        {
+          jobStationData?.find(
+            (jobStationData) =>
+              jobStationData?.id === employeeData?.jobStationId
+          )?.name
+        }
+      </td>
+      <td>
+        {
+          bloodGroupData?.find(
+            (bloodGroupData) =>
+              bloodGroupData?.id === employeeData?.bloodGroupId
+          )?.name
+        }
+      </td>
+      <td>
+        {
+          martialStatusData?.find(
+            (martialStatusData) =>
+              martialStatusData?.id === employeeData?.martialStatusId
+          )?.name
+        }
+      </td>
+      <td>
+        {
+          educationData?.find(
+            (educationData) => educationData?.id === employeeData?.educationId
+          )?.name
+        }
+      </td>
     </tr>
   ))
 
   useEffect(() => {
     getEmployeesData()
+    getEducationData()
+    getJobStationData()
+    getMartialStatusData()
+    getBloodGroupData()
   }, [])
 
   return (
-    <div className="flex flex-col">
+    <div className="flex flex-col space-y-4">
+      <h2 className="text-center font-semibold text-2xl mb-5">
+        Personel Listesi
+      </h2>
       <Table striped highlightOnHover withColumnBorders withBorder>
         <thead>{theadItems}</thead>
         <tbody>{trowItems}</tbody>
--- src/utils/store/EmployeesStore.js
+++ src/utils/store/EmployeesStore.js
@@ -3,9 +3,29 @@ import { axiosInstance } from '..'
 
 const useEmployeesStore = create((set) => ({
   employeesData: [],
+  educationData: [],
+  jobStationData: [],
+  martaialStatusData: [],
+  bloodGroupData: [],
   getEmployeesData: async () => {
     const response = await axiosInstance('employees')
     set({ employeesData: await response.data })
+  },
+  getEducationData: async () => {
+    const response = await axiosInstance('education')
+    set({ educationData: await response.data })
+  },
+  getJobStationData: async () => {
+    const response = await axiosInstance('jobStation')
+    set({ jobStationData: await response.data })
+  },
+  getMartialStatusData: async () => {
+    const response = await axiosInstance('martialStatus')
+    set({ martaialStatusData: await response.data })
+  },
+  getBloodGroupData: async () => {
+    const response = await axiosInstance('bloodGroup')
+    set({ bloodGroupData: await response.data })
   }
 }))
 
